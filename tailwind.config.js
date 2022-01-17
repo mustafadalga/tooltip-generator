6148126dcaa282d81c20b6bd7296443062222821
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-01-17 22:51:15 +0300

    Configure tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens:{
+        '3xl': '1680px',
+      }
+    },
+  },
+  plugins: [],
+}
