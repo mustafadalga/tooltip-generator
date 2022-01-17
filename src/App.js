bb198166e5103df8fafab7adaed7c659b5115881
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-01-17 22:57:27 +0300

    Structure project files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import SideBar from "./components/SideBar";
+import Codes from "./components/Codes";
+import Preview from "./components/Preview";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App w-full h-screen	max-w-screen-3xl mx-auto grid grid-rows-5 grid-cols-12">
+            <SideBar/>
+            <Preview/>
+            <Codes/>
+
+        </div>
+    );
 }
 
 export default App;
