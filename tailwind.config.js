cf8bc1c27198762917b1ce72ebfad7192e2792e6
Mustafa Dalga <mustafadalgaa@gmail.com>
2022-01-22 00:27:45 +0300

    add copy code animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,26 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      screens:{
-        '3xl': '1680px',
-      }
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            screens: {
+                '3xl': '1680px',
+            },
+            keyframes: {
+              'copied': {
+                'from': {
+                  bottom: '-100%',
+                },
+                'to': {
+                  bottom: '100%'
+                },
+              },
+            },
+            animation: {
+                'copied': 'copied 0.5s ease-out',
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
